959d3548a30a35044a03f03a4f59681598b15a77
Sanjay Srinivas <72558978+Sanjay567-coder@users.noreply.github.com>
2021-10-30 09:02:36 +0530

    Update final.py

--- final.py
+++ final.py
@@ -8,11 +8,20 @@ with open("drawf_star.csv","r")as f:
     csvreader=csv.reader(f)
     for row in csvreader:
         dataset1.append(row)
+        dataset1 = dataset1[dataset1['name'].notna()]
+        dataset1 = dataset1[dataset1['Distance'].notna()]
+        dataset1 = dataset1[dataset1['Mass'].notna()]
+        dataset1 = dataset1[dataset1['Radius'].notna()]
 
 with open("bright_stars.csv","r")as f:
     csvreader=csv.reader(f)
     for row in csvreader:
         dataset2.append(row)
+        dataset2 = dataset2[dataset2['Star_name'].notna()]
+        dataset2 = dataset2[dataset2['Distance'].notna()]
+        dataset2 = dataset2[dataset2['Mass'].notna()]
+        dataset2 = dataset2[dataset2['Radius'].notna()]
+        dataset2 = dataset2[dataset2['Luminosity'].notna()]
 
 headers1=dataset1[0]
 star_data1=dataset1[1:]
@@ -25,7 +34,7 @@ for i in star_data1:
     star_data.append(i)
 for a in star_data2:
     star_data.append(a)
-with open("final2.csv","a+")as f:
+with open("final3.csv","a+")as f:
     csvwriter=csv.writer(f)
     csvwriter.writerow(headers)
     csvwriter.writerows(star_data)
